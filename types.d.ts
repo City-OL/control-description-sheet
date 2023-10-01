4ced40e17cf71562017abe5dff9cf75e2abea461
timmedia <contact@tim-media.com>
2023-10-01 03:16:47 +0200

    feat: add typescript types

---
+++ types.d.ts
@@ -0,0 +1,28 @@
+export default class ControlDescriptionSheet extends HTMLElement {
+  title: string;
+  course?: string;
+  distance?: string;
+  elevation?: string;
+  finalDistance?: string;
+}
+
+export default class ControlDescription extends HTMLElement {
+  code?: string;
+  which?: string;
+  feature?: string;
+  appearance?: string;
+  dimension?: string;
+  location?: string;
+  other?: string;
+}
+
+declare global {
+  interface Window {
+    ControlDescriptionSheet: typeof ControlDescriptionSheet;
+    ControlDescription: typeof ControlDescription;
+  }
+  interface HTMLElementTagNameMap {
+    "control-description-sheet": ControlDescriptionSheet;
+    "control-description": ControlDescription;
+  }
+}
